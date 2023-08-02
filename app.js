cbec3c8049e005e8319ab03b2fa5c51a32af43cc
Juan David Valencia <juandavalg@gmail.com>
2023-08-02 21:55:00 +0200

    Lab

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ const express = require('express');
 
 const hbs = require('hbs');
 const path = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
-const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -18,8 +16,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  fetch('https://api.punkapi.com/v2/beers')
+    .then(response => response.json())
+    .then(beers => {
+      res.render('beers', { beers });
+    });
+});
+
+app.get('/randomBeer', (req, res, next) => {
+  fetch('https://api.punkapi.com/v2/beers/random')
+    .then(response => response.json())
+    .then(randomBeer => {
+      res.render('randomBeer', { randomBeer })
+    });
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
